3556201527611d654de8d4232390a61518aece9e
王学成 <wxc252@hotmail.com>
2020-04-07 15:52:49 +0800

    增加请求和响应的接口方法

--- src/main/java/com/df4j/v2/base/rpc/Request.java
+++ src/main/java/com/df4j/v2/base/rpc/Request.java
@@ -1,4 +1,9 @@
 package com.df4j.v2.base.rpc;
 
+import java.util.Map;
+
 public interface Request {
+    String getGroupId();
+    String getFuncNo();
+    Map getRequestMap();
 }
--- src/main/java/com/df4j/v2/base/rpc/Response.java
+++ src/main/java/com/df4j/v2/base/rpc/Response.java
@@ -1,4 +1,7 @@
 package com.df4j.v2.base.rpc;
 
 public interface Response {
+    void setErrorNo(int errorNo);
+    void setErrorInfo(String errorInfo);
+    void setResult(Object result);
 }
